3639a384485739a499fef39be3242c2c9929d396
TijsDeBelie <Tijsdebelie1997@gmail.com>
2018-12-20 09:51:46 +0100

    Commentaar en oude lijnen code

--- BD_verkiezingen/index.py
+++ BD_verkiezingen/index.py
@@ -7,45 +7,50 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from scipy.stats import norm
+
 #import pylab as pl
 
 import json
 
+# VUL HIER DE GEWENSTE KIESKRING EN PARTIJ IN, PARTIJ HEEFT EEN NUMMER
+KieskringNaam = "Merchtem"
+Partijnummer = 2
 
 # variabele om requested data in te plaatsen
 kieskringdata = urllib.request.urlopen("http://www.rocre.be/verkiezingen/json.php?fields=kieskring,naamstemmen,naam,id,lijstnr&duplicates=false").read()
 
-
+# De data die we terugkeren gaan laden in JSON formaat
 data = json.loads(kieskringdata)
 
-#kieskring_naam = list()
+# Een nieuwe lokale lijst maken van de stemmen
 kieskring_stemmen = list()
 
+# Omdat de json data in een wrapper van results zit dit gaan vervangen zodat de code op volgende lijnen korter is.
 data = data["results"]
 
+# Gaan kijken voor de gekozen kieskring en partij of er data inzit en deze dan bijhouden in de lokale lijst
 for x in data:
-    if(x["kieskring"] == "Aalst" and x["lijstnr"] == "2"):
-        #kieskring_naam.append(x["naam"])
+    if(x["kieskring"] == KieskringNaam and int(x["lijstnr"]) == Partijnummer):
         kieskring_stemmen.append(int(x["naamstemmen"]))
-
-
-#mean = np.mean(kieskring_stemmen)
-#std = np.std(kieskring_stemmen)
-#print(np.std(kieskring_stemmen))
-#print(norm.pdf(kieskring_stemmen,mean,np.std(kieskring_stemmen)))
-
-
+# als er geen stemmen zijn moet het programma stoppen 
+if(len(kieskring_stemmen) == 0) :
+        print("ERROR")
+        exit()
+# de stemmen van klein naar groot gaan sorteren
 kieskring_stemmen = sorted(kieskring_stemmen)
-#print(kieskring_stemmen)
 
+# de Y as gaan bepalen adhv de normaalverdeling
+y = norm.pdf(kieskring_stemmen, np.mean(kieskring_stemmen), np.std(kieskring_stemmen))
+plt.plot(kieskring_stemmen,y,'-o')
 
-fit = norm.pdf(kieskring_stemmen, np.mean(kieskring_stemmen), np.std(kieskring_stemmen))
-plt.plot(kieskring_stemmen,fit,'-o')
 
 plt.xlabel("Aantal stemmen")
 plt.ylabel("Kansdichtheid")
-plt.title("Stelling 1")
+plt.title("Stelling 1 voor : " + KieskringNaam)
 
+# Een histogram tekenen om makkelijk te gaan kijken of het een normaalverdeling is
 plt.hist(kieskring_stemmen,density=True)
 plt.show()
+
+# We kunnnen duidelijk zien dat het geen normaalverdeling is
 
